2e1d954c84ece854f0a69954ce6d5eac1fb5f214
nguyen chi thanh <n.c.thanh0801@gmail.com>
2016-11-09 16:56:59 +0700

    chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      render_404
+    end
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = I18n.t(".Welcome to the Sample App!")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
 end
