d05f8b0db12345a74ae4d28a7adfaf6c1f9340b3
nguyen chi thanh <n.c.thanh0801@gmail.com>
2016-11-10 16:39:31 +0700

    U-S-D users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  protected
+  def render_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = I18n.t".please_log_in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
